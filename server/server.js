3c131224f08def76ede3b74a2e7a7ac7979409ae
Mikael Wallin <mikael.wallin@gmail.com>
2017-02-07 12:00:19 +0100

    add user model

--- server/server.js
+++ server/server.js
@@ -12,34 +12,72 @@ mongoose.connect( "mongodb://localhost:27017/TodoApp" )
 
 const Todo = mongoose.model( "Todo", {
   text: {
-    type: String
+    type     : String,
+    required : true,
+    minlength: 2,
+    trim     : true
   },
   completed: {
-    type: Boolean
+    type   : Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type   : Number,
+    default: null
   }
 })
 
 const newTodo = new Todo({
-  text       : "Call mom",
-  completed  : true,
-  completedAt: 3456789
+  text: "C   ",
 })
 
 
 
-newTodo.save()
+// newTodo.save()
+//   .then( ( doc ) => {
+
+//     console.log( "Saved todo:", doc )
+
+//   })
+//   .catch( ( error ) => {
+
+//     console.log( "Unable to save new todo", error )
+
+//   })
+
+
+
+const User = mongoose.model( "User", {
+  name: {
+    type     : String,
+    required : true,
+    minlength: 1,
+    trim     : true
+  },
+  email: {
+    type     : String,
+    required : true,
+    minlength: 1,
+    trim     : true
+
+  }
+})
+
+
+const newUser = new User({
+  name : "Mikael Wallin",
+  email: "mikael.wallin@gmail.com"
+})
+
+newUser.save()
   .then( ( doc ) => {
 
-    console.log( "Saved todo:", doc )
+    console.log( "Saved user:", doc )
 
   })
   .catch( ( error ) => {
 
-    console.log( "Unable to save new todo", error )
+    console.log( "Unable to save new user", error )
 
   })
-
 
