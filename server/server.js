3704736d51f3e8897068af79955717bc17c8f093
Mikael Wallin <mikael.wallin@gmail.com>
2017-02-12 14:58:29 +0100

    add patch route for updates

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ require( "dotenv-safe" ).load()
 const bodyParser = require( "body-parser" )
 const express    = require( "express" )
 const {ObjectID} = require( "mongodb" )
+const _          = require( "lodash" )
 
+// const format     = require( "date-fns/format" )
+// const svLocale   = require( "date-fns/locale/sv" )
 
 // Internals
 
@@ -43,6 +46,9 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use( bodyParser.json() )
+
+
+
 
 
 // *****************************************************************************
@@ -154,6 +160,65 @@ app.delete( "/todos/:id", ( req, res ) => {
 
 
 })
+
+
+app.patch( "/todos/:id", ( req, res ) => {
+
+  const todoID = req.params.id
+  const body = _.pick( req.body, ["text", "completed"] )
+
+
+  if ( !ObjectID.isValid( todoID ) ) {
+    return res.status( 400 ).send()
+
+  }
+
+
+  if ( _.isBoolean( body.completed ) && body.completed ) {
+    body.completedAt = new Date().getTime()
+
+    // Formatting should be handled on the client
+    // body.completedAt = format(
+    //   new Date(),
+    //   "YYYY-MM-DD HH:mm:ss",
+    //   {locale: svLocale}
+    // )
+
+
+  } else {
+    body.completed = false
+    body.completedAt = null
+
+  }
+
+
+  Todo.findByIdAndUpdate(
+    todoID,
+    { $set: body },
+    {new: true }
+  )
+    .then( ( todo ) => {
+
+      if ( !todo ) {
+        return res.status( 404 ).send()
+
+      }
+
+      res.status( 200 ).send({todo})
+
+    })
+    .catch( ( error ) => {
+
+      res.status( 500 ).send()
+
+      console.log( error )
+
+    })
+
+
+})
+
+
 
 
 
